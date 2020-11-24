ae2d1e01ff81dc917ddd7e37165ed30607f40546
bruno fialho <fialho.bruno@live.fr>
2020-11-24 11:45:41 +0100

    creation des routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+
+  get "/cars/:id",    to: "cars#show", as: :car
+  get "cars",         to: "cars#index"
+  get "cars/new",     to: "cars#new", as: :new_car
+  post "cars",        to: "cars#create"
+
+  get "bookings/new", to: "bookings#new", as: :new_booking
+  post "bookings"     to: "booking#create"
+
+  get "bookings/:id/edit" to: "bookings#edit" as: :edit_bookings
+  patch "bookings/:id" to: "bookings#update"
+
+  delete "bookings/:id" to: "booking#destroy"
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
