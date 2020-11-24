385895142957ac5c8bee0b69832f6463b8392c3d
iliass benabderrahmane <iliass74@hotmail.fr>
2020-11-24 18:33:19 +0100

    ajouter car dans la seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,17 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
 
+puts "cleaning database"
+
 Car.destroy_all
 User.destroy_all
 
+puts "create a user"
 user = User.create!(email: "iliass@mail.com", password: "123456", first_name: "iliass", last_name: "ben")
 
+puts "create a car"
+car = Car.create(name: "Mercedes", description: "a louer", price: "29", model: "classe a", user: user)
 
+puts "finished"
 
 
