49b2a5e294e86a771cc1ae703d655b54fde21178
iliass benabderrahmane <iliass74@hotmail.fr>
2020-11-24 18:22:37 +0100

    creer un user dans la seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Car.destroy_all
+User.destroy_all
+
+user = User.create!(email: "iliass@mail.com", password: "123456", first_name: "iliass", last_name: "ben")
+
+
+
+
