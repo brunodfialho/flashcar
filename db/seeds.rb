1a3a99a04f0be6cf511ced101e16da7fe21740ab
iliass benabderrahmane <iliass74@hotmail.fr>
2020-11-25 12:07:00 +0100

    creation 10 users et 10 cars

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,17 +7,17 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
 
-puts "cleaning database"
+puts "Cleaning database"
 
 Car.destroy_all
 User.destroy_all
 
-puts "create a user"
-user = User.create!(email: "iliass@mail.com", password: "123456", first_name: "iliass", last_name: "ben")
+puts "Create a user"
+# user = User.create!(email: "iliass@mail.com", password: "123456", first_name: "iliass", last_name: "ben")
 
-puts "create a car"
-car = Car.create(name: "Mercedes", description: "a louer", price: "29", model: "classe a", user: user)
-
-puts "finished"
-
-
+10.times do
+  user = User.create(email: Faker::Internet.email, password: Faker::Internet.password, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+  puts "Create a car"
+  car = Car.create(name: Faker::Vehicle.make, description: "A louer", price: Faker::Commerce.price(range: 20..70, as_string: true), model: Faker::Vehicle.model, user: user)
+end
+puts "Done"
